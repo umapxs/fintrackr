95b3b7ee537aae91c7cbcbef75feafc47bf9f8f1
Tiago Calado <1tiagocalado8@gmail.com>
2023-06-02 17:06:54 +0100

    [Transactions] Implement a basic version of transaction  addition

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class TransactionsController extends Controller
 {
-    //
+    public function index() {
+        return view('transactions.index');
+    }
+
+    public function create() {
+    }
 }
