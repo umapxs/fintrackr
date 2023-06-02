e524b4cd5926fc22fa3a63549880fe60c4b53d7b
Tiago Calado <1tiagocalado8@gmail.com>
2023-06-02 09:39:52 +0100

    [Dashboard] Add accurate number of Total Banking Accounts

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Account;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard');
+        // Get the currently authenticated user
+        $user = Auth::user();
+
+        // Count the total accounts for the authenticated user
+        $totalAccounts = $user->accounts()->count();
+
+        return view('dashboard', ['totalAccounts' => $totalAccounts]);
     }
 }
