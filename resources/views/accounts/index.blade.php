22f13f934fe88e7a645af2c19e4e8a0d0b8312c5
Tiago Calado <1tiagocalado8@gmail.com>
2023-06-12 21:47:04 +0100

    [Categories] Add confirmation to Category deletion

--- resources/views/accounts/index.blade.php
+++ resources/views/accounts/index.blade.php
@@ -28,18 +28,13 @@
                                                 </label>
                                                 <div class="mt-6 relative">
                                                     <textarea name="input_text" class="p-6 w-full border-gray-200 text-md focus:outline-none focus:ring-black shadow-sm mt-1 block" id="input_text" rows="4" maxlength="50" placeholder="What is the Banking Account name?"></textarea>
-                                                        <p id="char_count" class="text-sm text-black mt-2 bottom-0 left-0" style="color: black;">0/50 Characters</p>
+                                                    <p id="char_count" class="text-sm text-black mt-2 bottom-0 left-0" style="color: black;">0/50 Characters</p>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
 
                                     <div class="flex items-center justify-end px-4 py-3 text-right sm:px-6 sm:rounded-bl-md sm:rounded-br-md">
-
-                                        <div x-data="{ shown: false, timeout: null }" x-init="window.livewire.find('ktTV58Y40TMRKelpGBgr').on('saved', () => { clearTimeout(timeout); shown = true; timeout = setTimeout(() => { shown = false }, 2000);  })" x-show.transition.out.opacity.duration.1500ms="shown" x-transition:leave.opacity.duration.1500ms="" style="display: none;" class="text-sm text-gray-600 mr-3">
-                                            Saved.
-                                        </div>
-
                                         <button type="submit" class="inline-flex items-center px-4 py-2 mb-4 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" wire:loading.attr="disabled" wire:target="photo">
                                             Save
                                         </button>
@@ -54,22 +49,26 @@
                             </div>
                         @else
                             @foreach ($accounts as $account)
-                                <a href="{{ route('accounts.show', ['id' => $account->id]) }}">
-                                    <div class="px-4 py-5 bg-white sm:p-6 shadow sm:rounded-tl-md sm:rounded-tr-md mt-8">
-                                        <div class="flex items-center justify-between">
-                                            <p class="font-thin">{{ $account->title }}</p>
+                            <div class="px-4 py-5 bg-white sm:p-6 shadow sm:rounded-tl-md sm:rounded-tr-md mt-8">
+                                <div class="flex items-center justify-between">
+                                            <a href="{{ route('accounts.show', ['id' => $account->id]) }}">
+                                                <p class="font-thin">{{ $account->title }}</p>
+                                            </a>
                                             <form action="{{ route('accounts.destroy', $account->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this account?');">
                                                 @csrf
                                                 @method('DELETE')
-                                                <button type="submit" class="text-red-600 mt-2">
+                                                <button type="button" class="text-red-600 mt-2 delete-button">
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5">
                                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
                                                     </svg>
                                                 </button>
+                                                <div class="password-input hidden">
+                                                    <input type="password" name="password" placeholder="Enter password" class="p-2 border border-gray-300 mt-2" />
+                                                    <button type="submit" class="px-3 py-2 bg-red-600 text-white mt-2">Confirm</button>
+                                                </div>
                                             </form>
                                         </div>
                                     </div>
-                                </a>
                             @endforeach
                         @endif
                     </div>
@@ -79,6 +78,16 @@
     </div>
 
     <script>
+        const deleteButtons = document.querySelectorAll('.delete-button');
+
+        deleteButtons.forEach((button) => {
+            button.addEventListener('click', function() {
+                const parentDiv = button.parentElement;
+                const passwordInput = parentDiv.querySelector('.password-input');
+                passwordInput.classList.toggle('hidden');
+            });
+        });
+
         const textarea = document.getElementById('input_text');
         const charCount = document.getElementById('char_count');
 
@@ -98,5 +107,4 @@
         // Trigger the input event on page load to update the character count
         textarea.dispatchEvent(new Event('input'));
     </script>
-
 </x-app-layout>
